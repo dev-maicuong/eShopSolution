3266a497195518e5d61cf9daa48d2d1bf4d55fb1
Hoang Mai Cuong <hoangmaicuong99@gmail.com>
2021-09-15 11:13:10 +0700

    design database

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 ## Technologies
 - ASP.NET Core 3.1
 - Entity Framework core 3.1
+## Install Packages
+- Microsoft.EntityFrameworkCore.SqlServer
+- Microsoft.EntityFrameworkCore.Design
+- Microsoft.EntityFrameworkCore.Tools
 ## Youtube tutorial
 ## How to configura and run
 ## How to contribute
---
+++ eShopSolution.Data/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using eShopSolution.Data.Enums;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public int SortOrder { get; set; }
+        public bool IsShowOnHome { get; set; }
+        public int? ParenId { get; set; }
+        public Status Status { get; set; }
+    }
+}
